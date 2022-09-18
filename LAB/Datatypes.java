d2995a17a57997cc008fceb12f28839496baa8f5
Monish <113083547+MONISHDECODE@users.noreply.github.com>
2022-09-18 23:00:26 +0530

    Add files via upload

---
+++ LAB/Datatypes.java
@@ -0,0 +1,58 @@
+/*
+NAME: MONISH.B
+USERNAME : AF0218356
+BATCH : CG2576
+MAIL ID : Monishofficialmail@gmail.com
+NUMBER : 8939507021
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
+
+//"WRITE A PROGRAM TO READ ALL DATATYPES"
+
+
+public class Datatypes 
+{
+
+	public static void main(String[] args) 
+	{
+		String name="MONISH";
+		String a=name;
+		
+		int age=21;
+		int b=age;
+		
+		byte luckyNo=5;
+		byte c=luckyNo;
+		
+		short luckyNo2= 8;
+		short d=luckyNo2;
+		
+		char initial= 'B';
+		char e=initial;
+		
+		float error=0.01F;
+		float f=error;
+		
+		double mark= 99.999D;
+		double g=mark;
+		
+		boolean details= true;
+		boolean h=details;
+		
+		long ph= 12345678;
+		long i=ph;
+		
+		
+		System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f+"\n"+g+"\n"+h+"\n"+i);		
+
+	}
+
+}
+
+/*
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
