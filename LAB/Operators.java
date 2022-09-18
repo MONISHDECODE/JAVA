adc3b888d960152c0abdb1b28a49b1b5d50c453f
Monish <113083547+MONISHDECODE@users.noreply.github.com>
2022-09-18 18:44:52 +0530

    Add files via upload

---
+++ LAB/Operators.java
@@ -0,0 +1,104 @@
+
+/*
+NAME: MONISH.B
+USERNAME : AF0218356
+BATCH : CG2576
+MAIL ID : Monishofficialmail@gmail.com
+NUMBER : 8939507021
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
+
+//"WRITE A PROGRAM TO COVER ALL TYPES OF OPERATORS"
+
+	
+// ASSIGNMENT OPERATOR, MULTIPLE ASSIGMENT
+class  Assignment {
+	public static void main(String[] args) 
+	{
+        int x;
+        x=7;
+        x+=5; //x=x+5
+        x-=4;//x=x-4
+        System.out.println("x value is "+x);
+	}
+}
+// ARITHEMETIC OPERATOR +,-,*,%
+class Arthimetic{
+	public static void main(String[] args) 
+	{
+		int a,b,c,d;
+		a=12;
+		b=5;
+		c=a+b;
+		d=a-b;
+		System.out.print(c+"  "+d);
+    }
+}
+// UNARY OPERATOR ++,--
+class  Unary{
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+		a=5;
+		b=++a;
+		System.out.println(a+" "+b);
+	}
+}
+// TERNARY OPERATOR ?,:
+class Ternary {
+	public static void main(String[] args) 
+	{
+		int x,y,z;
+		x=10;
+		y=20;
+		z=(x>y) ? x : y;
+		System.out.println("z value is "+ z);
+	}
+}
+
+// BITWISE OPERATOR &,|
+class Bitwise {
+    public static void main(String[] args) 
+	{
+		int a,b;
+		a=10;
+		b=2;
+        System.out.println(a&b);
+    }
+}
+
+// LOGICAL OPERATORS &&,||
+class Logical {
+    public static void main(String[] args) 
+	{
+		int a,b;
+		a=20;
+		b=10;
+		boolean flag;
+		flag=(a>10) && (b>10);
+        System.out.println("flag value "+flag);
+	}
+}
+
+// RELATIONAL OPERATORS<,>
+class Relational{
+    public static void main(String[] args) 
+	{
+		int a,b;
+		a=20;
+		b=10;
+		boolean b1,b2;
+		b1=(a>b);
+		b2=(a<b);
+        System.out.println(b1+" "+b2);
+	}
+}
+	
+
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+
+
