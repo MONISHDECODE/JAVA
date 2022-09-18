369bb272b95c4bef36cd389e813a450ac54e16f1
Monish <113083547+MONISHDECODE@users.noreply.github.com>
2022-09-19 03:51:09 +0530

    Add files via upload

---
+++ LAB/15th SEPT Assignment/Reverse.java
@@ -0,0 +1,31 @@
+/*
+NAME: MONISH.B
+USERNAME : AF0218356
+BATCH : CG2576
+MAIL ID : Monishofficialmail@gmail.com
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
+
+//WRITE A PROGRAM TO PRINT REVERSE NUMBERS
+
+public class Reverse
+{
+  public static void main(String args[]) 
+  {
+  int number=12345, reverse=0; 
+  
+  while(number!=0)
+  {
+      int remainder=number%10;
+      reverse= reverse*10+remainder;
+      number= number/10;
+  }
+      System.out.println("THE REVERSE OF THE GIVEN NUMBER IS: "+reverse);
+  }
+}
+
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
