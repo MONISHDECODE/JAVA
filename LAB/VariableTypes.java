31fe826a9f4f458ad979547f0a258820c9c2dafb
Monish <113083547+MONISHDECODE@users.noreply.github.com>
2022-09-18 23:19:57 +0530

    Add files via upload

---
+++ LAB/VariableTypes.java
@@ -0,0 +1,35 @@
+/*
+NAME: MONISH.B
+USERNAME : AF0218356
+BATCH : CG2576
+MAIL ID : Monishofficialmail@gmail.com
+NUMBER : 8939507021
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
+
+//WRITE A PROGRAM TO DEMO ALL TYPES OF VARIABLES
+
+class VariableTypes
+{
+     int a=100; //INSTANCE VARIABLE
+     static int b=200; //STATIC VARIABLE
+     public int m1()
+     {
+          int c = 300; //LOCAL VARIABLE
+          System.out.println("c = "+c);
+          return 0;
+     }
+	 
+     public static void main(String args[])
+     {
+          VariableTypes v = new VariableTypes ();
+          System.out.println("a = "+v.a+"  b = "+b);
+          v.m1(); //INVOKE THE m1 METHOD
+     }
+}
+
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
