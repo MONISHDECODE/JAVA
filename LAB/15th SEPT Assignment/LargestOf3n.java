0ec74a6be7d33d07ff2a2bb541d7edceeaa0f8e3
Monish <113083547+MONISHDECODE@users.noreply.github.com>
2022-09-19 03:07:36 +0530

    Add files via upload

---
+++ LAB/15th SEPT Assignment/LargestOf3n.java
@@ -0,0 +1,47 @@
+/*
+NAME: MONISH.B
+USERNAME : AF0218356
+BATCH : CG2576
+MAIL ID : Monishofficialmail@gmail.com
+NUMBER : 8939507021
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
+
+//WRITE A PROGRAM TO FIND THE LARGEST OF 3 NUMBERS
+
+import java.util.Scanner;
+
+public class LargestOf3n 
+{
+
+	public static void main(String[] args) 
+	{
+	  System.out.println("enter the three numbers");
+      Scanner obj= new Scanner(System.in);
+	  
+      int a = obj.nextInt();
+      int b = obj.nextInt(); 
+      int c = obj. nextInt();
+      if(a>b && a>c)
+      {
+    	  System.out.println("A IS GREATER THAN ALL");
+      }
+      else if(b>a && b>c) 
+	  {
+    	  
+    	  System.out.println(" B IS GREATER THAN ALL");
+      }
+      
+      else 
+	  {
+    	  System.out.println(" C IS GREATER THAN ALL");
+      }
+	}
+
+}
+
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
