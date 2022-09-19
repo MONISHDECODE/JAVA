dd6521851660fbbdb811fb1f4db45f989f65a8ca
Monish <113083547+MONISHDECODE@users.noreply.github.com>
2022-09-19 09:45:09 +0530

    Add files via upload

---
+++ LAB/15th SEPT Assignment/Pattern.java
@@ -0,0 +1,41 @@
+/*
+NAME: MONISH.B
+USERNAME : AF0218356
+BATCH : CG2576
+MAIL ID : Monishofficialmail@gmail.com
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
+
+//WRITE A PROGRAM TO PRINT GIVEN PATTEREN 
+/*
+
+*****
+****
+***
+**
+*
+
+*/
+
+public class Pattern
+{
+	public static void main(String args[]) 
+	{
+		int i,j;
+		for(i=5; i>=1; i--) 
+		{
+			for(j=1; j<=i; j++) 
+			{	
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
