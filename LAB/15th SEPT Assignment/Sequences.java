cb463f645d559ebf87c9f22a86a32a434e9886d5
Monish <113083547+MONISHDECODE@users.noreply.github.com>
2022-09-19 09:33:44 +0530

    Add files via upload

---
+++ LAB/15th SEPT Assignment/Sequences.java
@@ -0,0 +1,52 @@
+/*
+NAME: MONISH.B
+USERNAME : AF0218356
+BATCH : CG2576
+MAIL ID : Monishofficialmail@gmail.com
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
+
+// WRITE A PROGRAM TO DISPLAY 
+public class Sequences 
+{
+
+	public static void main(String[] args) 
+	{
+		int i;
+		for(i=1; i<=30; i=i+3) // a
+		{
+			System.out.println(i);
+		}
+		
+        // b
+		for (i=1; i<600; i=i*2) 
+		{
+			
+			System.out.println(i);
+		}
+		
+		// c
+	    int first= 0;
+		int second = 1;
+	    int next ;
+			    
+		for( i=1; i<11; i++)
+		{
+		System.out.println(first);
+		next=first+second;
+	    first=second;
+	    second= next;
+		}
+			
+
+
+	}
+	
+
+}
+
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
