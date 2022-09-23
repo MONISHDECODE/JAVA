6938b95216fdf6813238342736796fe89c5a3f7d
Monish <113083547+MONISHDECODE@users.noreply.github.com>
2022-09-23 09:40:22 +0530

    Add files via upload

---
+++ LAB/22nd SEPT Assignment/ArrayElements.java
@@ -0,0 +1,44 @@
+/*
+NAME: MONISH.B
+USERNAME : AF0218356
+BATCH : CG2576
+MAIL ID : Monishofficialmail@gmail.com
+GitHubLink : https://github.com/MONISHDECODE/JAVA
+*/
+
+
+//FIND MAXIMUM OF ARRAY ELEMENTS, MINIMUM OF ARRAY ELEMENT  USING METHODS. 
+
+class ArrayElements
+ {
+	 public static void main(String[] args)
+	 {
+		 int a[]={10,20,30,40,50};
+		 int max=x[0];
+		 for(int i=1; i<x.length;i++)
+		 {
+			 if(x[i]>max)
+			 {
+			 max=x[i];
+			 }
+		 }
+		 System.out.println("MAXIMUM ARRAY ELEMENT IS: "+max);
+
+         int min=x[0];
+		 for(int i=1; i<x.length;i++)
+		 {
+			 if(x[i]<min)
+			 {
+			 min=x[i];
+			 }
+		 }
+		  System.out.println("MINIMUM ARRAY ELEMENT IS: "+min);
+	 }
+ }
+ 
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+/*https://github.com/MONISHDECODE/JAVA*/
+
